3fc52a6e90da3442c1dec11f07655ea5edc22da2
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-03-04 15:19:31 +0700

    ADD:Header Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div>ok</div>;
+  return (
+    <div className="app">
+      <Header />
+      <h1>Mainboard</h1>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,145 @@
+import React from "react";
+import styled from "styled-components";
+import PinterestIcon from "@material-ui/icons/Pinterest";
+import { IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import TextsmsIcon from "@material-ui/icons/Textsms";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import FaceIcon from "@material-ui/icons/Face";
+
+const Header = () => {
+  return (
+    <Wrapper>
+      <LogoWrapper>
+        <IconButton>
+          <PinterestIcon />
+        </IconButton>
+      </LogoWrapper>
+      <HomePageButton>
+        <a href="/">HomePage</a>
+      </HomePageButton>
+      <FollowingButton>
+        <a href="/">Fllowing </a>
+      </FollowingButton>
+      <SearchWrapper>
+        <SearchBarWrapper>
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+          <form>
+            <input type="text" />
+            <button type="submit"></button>
+          </form>
+        </SearchBarWrapper>
+      </SearchWrapper>
+      <IconWrapper>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <IconButton>
+          <TextsmsIcon />
+        </IconButton>
+        <IconButton>
+          <FaceIcon />
+        </IconButton>
+        <IconButton>
+          <KeyboardArrowDownIcon />
+        </IconButton>
+      </IconWrapper>
+    </Wrapper>
+  );
+};
+
+export default Header;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 56px;
+  padding: 12px 4px 4px 16px;
+  background-color: white;
+  color: black;
+`;
+
+const LogoWrapper = styled.div`
+  .MuiSvgIcon-root {
+    color: #e60023;
+    font-size: 32px;
+    cursor: pointer;
+  }
+`;
+
+// Generic button
+const HomeCommonButtons = styled.div`
+  display: flex;
+  height: 48px;
+  min-width: 123px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 24px;
+  cursor: pointer;
+`;
+
+// Inherit styles from HomeCommonButtons
+const HomePageButton = styled(HomeCommonButtons)`
+  background-color: rgb(17, 17, 17);
+
+  a {
+    text-decoration: none;
+    color: white;
+    font-weight: 700;
+  }
+`;
+
+const FollowingButton = styled(HomeCommonButtons)`
+  background-color: white;
+
+  a {
+    text-decoration: none;
+    color: black;
+    font-weight: 700;
+  }
+
+  :hover {
+    background-color: #e1e1e1;
+  }
+`;
+
+const SearchWrapper = styled.div`
+  flex: 1;
+`;
+
+const SearchBarWrapper = styled.div`
+  display: flex;
+  height: 48px;
+  width: 100%;
+  border-radius: 50px;
+  border: none;
+  padding-left: 10px;
+  background-color: #efefef;
+
+  form {
+    display: flex;
+    flex: 1;
+  }
+
+  form > input {
+    background-color: transparent;
+    font-size: 16px;
+    border: none;
+    outline: none;
+    margin-left: 5px;
+  }
+
+  form input :focus {
+    border: none;
+  }
+
+  form > button {
+    display: none;
+    outline: none;
+  }
+`;
+
+const IconWrapper = styled.div``;
