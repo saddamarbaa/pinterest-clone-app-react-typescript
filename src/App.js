153d206827bc8a9d45570bf7411674f9a5f4ce3d
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-03-04 08:26:01 +0700

    ADD:restart the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import "./App.css";
+import React from "react";
 
-import Header from "./Header.js";
-
-function App() {
-  return (
-    <div className="app">
-      <Header className="App-header"></Header>
-    </div>
-  );
-}
+const App = () => {
+  return <div>ok</div>;
+};
 
 export default App;
