a81470706d91090a246740367ee075b764222f58
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-03-06 05:36:23 +0700

    ADD:Fix uniqueId problem

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -1,14 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
-const Pin = () => {
+const Pin = ({ urls }) => {
   return (
     <Wrapper>
       <Container>
-        <img
-          src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80"
-          alt=""
-        />
+        <img src={urls?.regular} alt="" />
       </Container>
     </Wrapper>
   );
